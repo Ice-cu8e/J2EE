c57ac584946842ed5d954e9f79c14381449fd8b5
Yann <cloarecyann@gmail.com>
2019-05-17 09:20:10 +0200

    Tests unitaires PASS

--- src/main/java/fr/epsi/jeeProject/dao/IBlogDao.java
+++ src/main/java/fr/epsi/jeeProject/dao/IBlogDao.java
@@ -11,6 +11,7 @@ public interface IBlogDao {
 
 	Blog getBlog(Integer id);
 	void createBlog(Blog blog) throws SQLException;
+	void createBlogWithId(Blog blog) throws SQLException;
 	void updateBlog(Blog blog) throws SQLException;
 	void deleteBlog(Blog blog) throws SQLException;
 	void deleteBlogFromId(int id,Utilisateur user) throws SQLException;
